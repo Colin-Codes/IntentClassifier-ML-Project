50d5834060c26dc720aa359ba7071b697ca0bd06
Colin <cy16aan@herts.ac.uk>
2019-09-21 16:30:11 +0100

    RNN Classifier

---
+++ IntentionClassifier/RNNClassifier.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+
+#Import the data, split into X, y
+ClassEmailPairs = pd.read_csv('data/trainingset.csv', sep=',', engine='python', quotechar='"')
+X = ClassEmailPairs['Email']
+y = ClassEmailPairs['Class']
+
+#Create the word embedding
+Vectorizer = CountVectorizer(max_features=100, stop_words='english')
+BagOfWords = Vectorizer.fit(X)
+X_train = BagOfWords.transform(X)
+
+#List of unique intentions
+intents = ClassEmailPairs['Class'].unique()
+
+print(intents)
+
+#Score the model
+scores = []
+for i in range(1,25):
+    model = KNeighborsClassifier(n_neighbors=i)
+    CVScores = cross_val_score(model, X_train, y, cv=25)
+    scores.append(CVScores.mean())
+
+plt.scatter(range(1,25), scores)
+plt.show()
